e0baca15373d40e405a2e889caf3a3ff8416f594
Corrigan Newsome <cnewsome2@cnm.edu>
2020-12-16 08:29:31 -0700

    fixed some formatting on page.php and style.css

--- page.php
+++ page.php
@@ -19,16 +19,18 @@ while (have_posts()) {
   <div class="container container--narrow page-section">
 
   <?php
-$theParent = wp_get_post_parent_id(get_the_id());
+$theParent = wp_get_post_parent_id(get_the_ID());
+    if (wp_get_post_parent_id(get_the_ID())) {?>
 
-    if ($theParent) {?>
-        <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i>Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title()?></span></p>
+<div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span></p ?>
     </div>
-
-    <?php }
+   <?php }
 
     ?>
+
+
+
 
 <!--
     <div class="page-links">
