2c1dea8382d77f136b69cf887676c74dc3997155
Corrigan Newsome <cnewsome2@cnm.edu>
2020-12-17 15:23:42 -0700

    added array to page.php

--- page.php
+++ page.php
@@ -32,15 +32,25 @@ $theParent = wp_get_post_parent_id(get_the_ID());
   
     ?>
 
-    <!--
+
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+            <?php 
+            if ($theParent) {
+                $findChildrenOf = $theParent;
+            }else {
+                $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages(array(
+                "title_li" => NULL, 
+                "child_of" => $findChildrenOf,
+                
+            ));
+            ?>
+        </ul>
     </div>
-    -->
+
 
     <div class="generic-content">
         <?php the_content();?>
