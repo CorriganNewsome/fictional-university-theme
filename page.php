a4f6bb10d0f01026dea8385ec9a033cad365e893
Corrigan Newsome <cnewsome2@cnm.edu>
2020-12-17 15:59:10 -0700

    added if statement for menu without parent or child pages.

--- page.php
+++ page.php
@@ -32,10 +32,19 @@ $theParent = wp_get_post_parent_id(get_the_ID());
   
     ?>
 
+    <?php
+    $testArray = get_pages(array(
+        "child_of" => get_the_ID()
+    ));
+    
+if ($theParent or $testArray) { ?>
 
+    }
     <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent)?>">
+                <?php echo get_the_title($theParent);
+                ?></a></h2>
+        <ul class=" min-list">
             <?php 
             if ($theParent) {
                 $findChildrenOf = $theParent;
@@ -45,11 +54,14 @@ $theParent = wp_get_post_parent_id(get_the_ID());
             wp_list_pages(array(
                 "title_li" => NULL, 
                 "child_of" => $findChildrenOf,
+                "sort_column" => "menu_order"
                 
             ));
             ?>
         </ul>
     </div>
+
+    <?php } ?>
 
 
     <div class="generic-content">
